baeff1bf6f58b72dadcf05ffd5a61d2d99b84eed
Audrey ELIE <audrey@familleelie.fr>
2024-09-05 17:25:37 +0200

    en cours: menu hamburger

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    CommonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
