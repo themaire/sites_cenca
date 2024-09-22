c447d5c00fd8d5a01d73ffacb29c2e6e7a4a4e06
AudreyELIE <audrey@familleelie.fr>
2024-09-22 13:58:35 +0200

    detail-description terminer

--- src/app/backendAdress.ts
+++ src/app/backendAdress.ts
@@ -1,3 +1,3 @@
-// export let backendAdress: string = "http://192.168.0.29:8889/"; // Télétravail
+export let backendAdress: string = 'http://192.168.0.29:8889/'; // Télétravail
 
-export let backendAdress: string = 'http://192.168.1.50:8889/'; // Bureau;
+// export let backendAdress: string = 'http://192.168.1.50:8889/'; // Bureau;
