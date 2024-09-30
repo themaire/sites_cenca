784d1d41db2ffce0baddc848e6be6e7fac65b662
Audrey ELIE <audrey@familleelie.fr>
2024-09-30 16:30:12 +0200

    transfert

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  private formBuilder = Inject(FormBuilder);
+
+  loginFormGroup = this.formBuilder.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  invalidCredentials = false;
+
+  login() {}
+}
