5fec60a0ac488af92f2eaa47941adc50244d97a1
Themaire <n.elie@me.com>
2024-10-25 13:56:32 +0200

    Update backendAdress.ts
    Trompé de port précédement

--- src/app/backendAdress.ts
+++ src/app/backendAdress.ts
@@ -1,5 +1,5 @@
 // export const backendAdress: string = 'http://192.168.0.29:8889/'; // Télétravail
 
-// export const backendAdress: string = 'http://192.168.1.50:8889/'; // Bureau;
+// export const backendAdress: string = 'http://192.168.1.50:8889/'; // Bureau
 
-export const backendAdress: string = 'http://si-10.cen-champagne-ardenne.org:8885/'; // Internet
+export const backendAdress: string = 'http://si-10.cen-champagne-ardenne.org:8887/'; // Internet
