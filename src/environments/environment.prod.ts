739224a1b4483433b1f4639610724d5c4b748e8a
Themaire <n.elie@me.com>
2025-10-14 11:43:24 +0200

    Ajout de la gestion de la propriété 'windows' pour déterminer l'URL de l'API et le séparateur de chemin.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,16 @@
 export const environment = {
+    windows: false,
     production: true,
+    
     apiUrl: 'https://si-10.cen-champagne-ardenne.org:8889/',
+    apiLocalUrl: 'http://localhost:8889/',
+    get apiBaseUrl() {
+        // Si la propriété windows est à true, on utilise apiLocalUrl, sinon apiUrl
+        return this.windows ? this.apiLocalUrl : this.apiUrl;
+    },
+
+    get pathSep() {
+        // Si la propriété windows est à true, on utilise '\\', sinon '/'
+        return this.windows ? '\\' : '/';
+    }
   };
